3477f5bad408d92a0df51f1c3d241c123f7c81be
nikan <nikanemami7@gmail.com>
2024-06-01 11:49:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+    {
+        name: "stick",
+        power: 5
+    },
+    {
+        name: "dagger",
+        power: 30
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100
+    }
+];
 const locations = [
     {
         name: "town square",
@@ -29,6 +47,12 @@ const locations = [
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
 ];
 
@@ -56,10 +80,11 @@ function goTown() {
 };
 
 function goStore() {
+    update(locations[1]);
 };
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 };
 
 function fightDragon() {
@@ -67,8 +92,29 @@ function fightDragon() {
 };
 
 function buyHealth() {
-
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    };
 };
 
-function buyWeapon() { };
+function buyWeapon() {
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeaponIndex++;
+        goldText.innerText = gold;
+        let newWeapon = weapons;
+        text.innerText = "You now have a new weapon.";
+        newWeapon = weapons[currentWeaponIndex];
+        newWeapon = weapons[currentWeaponIndex].name;
+    };
+};
+
+function fightSlime() { };
+
+function fightBeast() { };
 
